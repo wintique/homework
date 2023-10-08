ee1b68a3d080770fec66210d6c3ebd3054988ccd
Виталий Водолазов <0wintick0@gmail.com>
2023-10-08 22:47:35 +0300

    Выводит сообщение на выбранном языке

---
+++ hello.pas
@@ -0,0 +1,8 @@
+﻿begin
+  var lang:= ReadInteger('1.Русский 2.English. 3.Español');
+  case lang of
+    1: Print('Привет, мир!');
+    2: Print('Hello, world!');
+    else Print('Hola mundo!');
+  end;
+end.
